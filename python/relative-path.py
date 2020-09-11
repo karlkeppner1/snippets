a076303c432dbe60b39c379579522e3f17b03447
Karl Keppner <kkeppner@nuskin.com>
2020-09-11 16:41:32 -0600

    more relative path methods

--- python/relative-path.py
+++ python/relative-path.py
@@ -3,3 +3,5 @@ import os
 working_dir = os.getcwd()
 file_path = os.path.join(working_dir, input('Please enter relative path to file.\n'))
 
+dirname = os.path.dirname(__file__)
+zones_path = os.path.join(dirname, 'zones')
